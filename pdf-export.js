2d0aa4b805a17d11bae15930da4d309ce7d64df6
Matthew Nestor <matthew.nestor@clickfunnels.com>
2024-04-26 09:06:39 -0400

    Attempt to support M1 Macbook PDF build. Still Timeout

--- pdf-export.js
+++ pdf-export.js
@@ -7,11 +7,11 @@ const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
 const html = await render(resume, theme)
 
 const browser = await puppeteer.launch({
-    args: ['--no-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
 })
 const page = await browser.newPage()
 
 //await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.goto('file:///app/index.html')
+await page.goto('file:///app/index.html', {waitUntil: 'load', timeout: 0})
 await page.pdf({ path: 'resume.pdf', format: 'letter', printBackground: true })
 await browser.close()

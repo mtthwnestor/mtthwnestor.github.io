425028fbbe99942d010b4510901b6b7c83722340
Matthew Nestor <matt@piperservers.net>
2024-08-20 18:55:11 -0400

    Use NodeJS 'path' to provide relative paths

--- pdf-export.js
+++ pdf-export.js
@@ -4,6 +4,8 @@ import * as theme_ats from '@jsonresume/jsonresume-theme-class'
 import puppeteer from 'puppeteer'
 import { render } from 'resumed'
 
+const path = require('path');
+
 const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
 const html = await render(resume, theme)
 const html_ats = await render(resume, theme_ats)
@@ -14,8 +16,8 @@ const browser = await puppeteer.launch({
 const page = await browser.newPage()
 
 //await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.goto('file://./index.html', {waitUntil: 'load', timeout: 0})
+await page.goto(`file:${path.join(__dirname, 'index.html')}`, {waitUntil: 'load', timeout: 0})
 await page.pdf({ path: 'resume.pdf', format: 'letter', printBackground: true })
-await page.goto('file://./matthew-nestor.html', {waitUntil: 'load', timeout: 0})
+await page.goto(`file:${path.join(__dirname, 'matthew-nestor.html')}`, {waitUntil: 'load', timeout: 0})
 await page.pdf({ path: 'matthew-nestor.pdf', format: 'letter', printBackground: true })
 await browser.close()

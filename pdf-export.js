d8a95ac321cb0014acfcdfebfdfeeb003e39dc94
Matthew Nestor <matt@piperservers.net>
2024-08-20 19:44:40 -0400

    Use process.cwd for current path

--- pdf-export.js
+++ pdf-export.js
@@ -1,10 +1,10 @@
 import { promises as fs } from 'fs'
+import * as path from 'path'
+import { cwd } from 'process'
 import * as theme from 'jsonresume-theme-even'
 import * as theme_ats from '@jsonresume/jsonresume-theme-class'
 import puppeteer from 'puppeteer'
 import { render } from 'resumed'
-
-const path = require('path');
 
 const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
 const html = await render(resume, theme)
@@ -16,8 +16,8 @@ const browser = await puppeteer.launch({
 const page = await browser.newPage()
 
 //await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.goto(`file:${path.join(__dirname, 'index.html')}`, {waitUntil: 'load', timeout: 0})
+await page.goto(`file:${path.join(cwd(), 'index.html')}`, {waitUntil: 'load', timeout: 0})
 await page.pdf({ path: 'resume.pdf', format: 'letter', printBackground: true })
-await page.goto(`file:${path.join(__dirname, 'matthew-nestor.html')}`, {waitUntil: 'load', timeout: 0})
+await page.goto(`file:${path.join(cwd(), 'matthew-nestor.html')}`, {waitUntil: 'load', timeout: 0})
 await page.pdf({ path: 'matthew-nestor.pdf', format: 'letter', printBackground: true })
 await browser.close()
